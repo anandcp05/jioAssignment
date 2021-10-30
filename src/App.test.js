13ac4de4f097eedf66b291784e4ba16bf7f459d3
anandvizion <90131334+anandvizion@users.noreply.github.com>
2021-10-31 01:21:08 +0530

    added testcases

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Day is Beautiful/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('Should render profilename ', () => {
+  render(<App />);
+  const profileName = screen.getByText(/Anand C.P/i);
+  expect(profileName).toBeInTheDocument();
 });
